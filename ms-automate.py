d63c1e3d6eff886d84e1020840917d35614be9fd
Oliver Reynolds <oli_kester@zoho.com>
2020-07-27 20:39:53 +0100

    Imported into PyCharm

--- ms-automate.py
+++ ms-automate.py
@@ -1,4 +1,7 @@
-import pyautogui, sys, random, time, keyboard
+import pyautogui
+import random
+import time
+import keyboard
 
 # give us time to head over to MSRD
 time.sleep(10)
@@ -6,16 +9,16 @@ time.sleep(10)
 keepLooping = True
 
 while keepLooping:
-    randWaitTime = random.randint(1,10)
-    randXOffset = random.randint(-50,50)
-    randYOffset = random.randint(-50,50)
-    randMoveDuration = random.randint(1,5)
-    
+    randWaitTime = random.randint(1, 10)
+    randXOffset = random.randint(-50, 50)
+    randYOffset = random.randint(-50, 50)
+    randMoveDuration = random.randint(1, 5)
+
     mouseX, mouseY = pyautogui.position()
 
-    #TODO enforce bounds on mouse position     
+    # TODO enforce bounds on mouse position
 
-    pyautogui.moveRel(randXOffset,randYOffset,randMoveDuration)
+    pyautogui.moveRel(randXOffset, randYOffset, randMoveDuration)
 
     time.sleep(randWaitTime)
 
